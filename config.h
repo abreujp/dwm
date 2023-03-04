4bc24460d3651f6bde5695dcb834b7e218716eb5
jp7webdesign <jpaulo6ba@gmail.com>
2023-03-04 16:38:32 -0300

    change font

--- config.h
+++ config.h
@@ -21,8 +21,8 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444"; 
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#bd93f9";
+static char selbgcolor[]            = "#bd93f9";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },

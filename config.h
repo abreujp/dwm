64edea997089a01f13be783f4cc2d8bd55a57b98
jp7webdesign <jpaulo6ba@gmail.com>
2023-03-25 15:30:25 -0300

    change color dmenu

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const char col_gray4[]       = "#d79921";
 static const char col_cyan[]        = "#fbf1c7";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray3 },
+	[SchemeNorm] = { col_cyan, col_gray1, col_gray3 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_gray4  },
 };
 
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_cyan, "-nf", col_gray4, "-sb", col_gray4, "-sf", col_cyan, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {

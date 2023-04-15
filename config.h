1b58725ba256b28a3b87653dd615a6256b8b0582
jp7webdesign <jpaulo6ba@gmail.com>
2023-04-15 16:21:55 -0300

    change name of colors

--- config.h
+++ config.h
@@ -15,15 +15,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#b8bb26";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_gray4[]       = "#b8bb26";
-static const char col_cyan[]        = "#b8bb26";
+
+static const char normfgcolor[]       = "#ebdbb2";
+static const char normbgcolor[]       = "#282828";
+static const char normbordercolor[]   = "#282828";
+static const char selfgcolor[]        = "#282828";
+static const char selbgcolor[]        = "#b8bb26";
+static const char selbordercolor[]    = "#b8bb26";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_gray2  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor },
+
 };
 
 
@@ -71,7 +75,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "exitdwm.c"

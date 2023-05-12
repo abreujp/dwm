fdf58364d4ef5fcb8e0085f0dea7eae259562e4b
jp7webdesign <jpaulo6ba@gmail.com>
2023-05-12 14:27:38 -0300

    change size font

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=bold:size=13:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=13:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=13";
 
 static const char normfgcolor[]       = "#262626";
 static const char normbgcolor[]       = "#ffffff";

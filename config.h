74dd045c3b68124cb507eba1f35874b53739bd7d
jp7webdesign <jpaulo6ba@gmail.com>
2023-05-18 08:11:34 -0300

    change tags from icons to numbers

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const char normbgcolor[]       = "#ffffff";
 static const char normbordercolor[]   = "#ffffff";
 static const char selfgcolor[]        = "#ffffff";
 static const char selbgcolor[]        = "#0f68a0";
-static const char selbordercolor[]    = "#4B21B0";
+static const char selbordercolor[]    = "#0f68a0";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -31,7 +31,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰨊", "", "", "", "", "", "", "", "" };
+/* static const char *tags[] = { "󰨊", "", "", "", "", "", "", "", "" }; */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):

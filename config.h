583b4c37ae3932d68eb2c8f83c33417d0a134831
abreujp <jpaulo6ba@gmail.com>
2024-07-09 18:08:24 -0300

    Update font FiraCode and change terminal to wezterm

--- config.h
+++ config.h
@@ -14,9 +14,11 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=bold:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+/* static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=bold:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" }; */
+static const char *fonts[]          = { "FiraCode Nerd Font:style=Regular:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
+/* static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10"; */
+static const char dmenufont[]       = "FiraCode Nerd Font:size=10";
 
 static const char normfgcolor[]     = "#2E3440"; // Cinza escuro para texto normal
 static const char normbgcolor[]     = "#ECEFF4"; // Branco para fundo normal
@@ -88,7 +90,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "wezterm", NULL };
 static const char *rofi[]  = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *scrotselcmd[]  =  {"scrot", "-s", NULL };
 

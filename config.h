de5727aaf17a08d0e60bee3ae8e3bbeab36e68e4
abreujp <jpaulo6ba@gmail.com>
2024-07-10 07:47:22 -0300

    Update font FiraCode to Mono

--- config.h
+++ config.h
@@ -15,10 +15,10 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /* static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=bold:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" }; */
-static const char *fonts[]          = { "FiraCode Nerd Font:style=Regular:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "FiraCode Nerd Font Mono:style=Bold:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 
 /* static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10"; */
-static const char dmenufont[]       = "FiraCode Nerd Font:size=10";
+static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=10";
 
 static const char normfgcolor[]     = "#2E3440"; // Cinza escuro para texto normal
 static const char normbgcolor[]     = "#ECEFF4"; // Branco para fundo normal
